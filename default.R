b78e8ac2a1eb8fa9887affeeb555e83ca1142484
Josh Wozny <joshua.wozny@snhu.edu>
2022-09-17 17:13:27 -0400

    Add default.R file that loads standard packages

---
+++ default.R
@@ -0,0 +1,34 @@
+# Load packages -------------------------------
+# pavman: package manager
+# tidyverse: packages of packages
+#  	broom: Convert Statistical Objects into Tidy Tibbles
+#   cli: Helpers for Developing Command Line Interfaces
+#   crayon: Colored Terminal Output
+#   dbplyr: A 'dplyr' Back End for Databases
+#   dplyr:  A Grammar of Data Manipulation
+#   dtplyr: Data Table Back-End for 'dplyr'
+#   forcats: Tools for Working with Categorical Variables (Factors)
+#   ggplot2: Create Elegant Data Visualisations Using the Grammar of Graphics
+#   googledrive: An Interface to Google Drive
+#   googlesheets4: Access Google Sheets using the Sheets API V4
+#   haven: Import and Export 'SPSS', 'Stata' and 'SAS' Files
+#   hms: Pretty Time of Day
+#   httr: Tools for Working with URLs and HTTP
+#   jsonlite: Tools for Working with URLs and HTTP
+#   lubridate: Make Dealing with Dates a Little Easier
+#   magrittr: Make Dealing with Dates a Little Easier
+#   modelr: Modelling Functions that Work with the Pipe
+#   pillar: Coloured Formatting for Columns
+#   purrr: Functional Programming Tools
+#   readr: Read Rectangular Text Data
+#   readxl: Read Excel Files
+#   reprex: Prepare Reproducible Example Code via the Clipboard
+#   rlang: Prepare Reproducible Example Code via the Clipboard
+#   rstudioapi: Safely Access the RStudio API
+#   rvest: Easily Harvest (Scrape) Web Pages
+#   stringr: Simple, Consistent Wrappers for Common String Operations
+#   tibble: Simple Data Frames
+#   tidyr: Tidy Messy Data
+#   xml2: Parse XML
+# rio: A Swiss-Army Knife for Data I/O
+pacman::p_load(pacman,tidyverse,rio,datasets)
